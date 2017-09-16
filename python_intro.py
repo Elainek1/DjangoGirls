86a232b92917e4f61a42436c0bf19adcca03bdc9
Elaine Khuu <elainek1@vt.edu>
2017-09-16 18:26:54 +0000

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+print('Hello, Django girls!')
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+        
+hi("Ola")
